c8745d5cf754c98909a1f958ceec796af3cfa1f5
steev0412 <elisra0412@gmail.com>
2020-11-11 23:36:18 -0500

    base de datos

--- proyecto_flask/app/__init__.py
+++ proyecto_flask/app/__init__.py
@@ -1,9 +1,30 @@
 from flask import Flask
-from proyecto_flask.app.products.views import product
+
+from proyecto_flask.app.db import db, ma
+from proyecto_flask.conf.config import DevelopmentConfig
+from proyecto_flask.app.products.views import products
+
+ACTIVE_ENDPOINTS = [('/products', products)]
 
 
-app = Flask(__name__)
-app.register_blueprint(product)
+def create_app(config=DevelopmentConfig):
+    app = Flask(__name__)
+
+    app.config.from_object(config)
+
+    db.init_app(app)
+    ma.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    # register each active blueprint
+    for url, blueprint in ACTIVE_ENDPOINTS:
+        app.register_blueprint(blueprint, url_prefix=url)
+
+    return app
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app_flask = create_app()
+    app_flask.run()
--- proyecto_flask/app/products/views.py
+++ proyecto_flask/app/products/views.py
@@ -1,15 +1,90 @@
-from flask import Blueprint
+from flask import Blueprint, Response, request
+from http import HTTPStatus
 
-product = Blueprint('product', __name__, url_prefix='/products')
+from proyecto_flask.app.products.models import get_all_categories, create_new_category, \
+    get_all_products, get_product_by_id
 
+products = Blueprint('product', __name__, url_prefix='/products')
 
-@product.route('/<string:name>', methods=['GET'])
+EMPTY_SHELVE_TEXT = "Empty shelve!"
+PRODUCTS_TITLE = "<h1> Products </h1>"
+DUMMY_TEXT = "Dummy method to show how Response works"
+RESPONSE_BODY = {
+    "message": "",
+    "data": [],
+    "errors": [],
+    "metadata": []
+}
+
+@products.route('/<string:name>', methods=['GET'])
 def index(name):
     if name == "pygroup":
         return "<h1>ERROR! No se puede usar el nombre pygroup</h1>", 400
     else:
         return "<h1>Felicitaciones! Trabajo exitoso {}</h1>".format(name), 200
 
+
+@products.route('/categories')
+def get_categories():
+    categories = get_all_categories()
+    status_code = HTTPStatus.OK
+
+    if categories:
+        RESPONSE_BODY["message"] = "OK. Categories List"
+        RESPONSE_BODY["data"] = categories
+    else:
+        RESPONSE_BODY["message"] = "OK. No categories found"
+        RESPONSE_BODY["data"] = categories
+        status_code = HTTPStatus.NOT_FOUND
+
+    return RESPONSE_BODY, status_code
+
+
+@products.route('/add-category', methods=['POST'])
+def create_category():
+    RESPONSE_BODY["message"] = "Method not allowed"
+    status_code = HTTPStatus.METHOD_NOT_ALLOWED
+    if request.method == "POST":
+        data = request.json
+        category = create_new_category(data["name"])
+        RESPONSE_BODY["message"] = "OK. Category created!"
+        RESPONSE_BODY["data"] = category
+        status_code = HTTPStatus.CREATED
+
+    return RESPONSE_BODY, status_code
+
+
+@products.route('/')
+def get_products():
+    products_obj = get_all_products()
+
+    RESPONSE_BODY["data"] = products_obj
+    RESPONSE_BODY["message"] = "Products list"
+
+    return RESPONSE_BODY, 200
+
+
+@products.route('/product/<int:id>')
+def get_product(id):
+    product = get_product_by_id(id)
+
+    RESPONSE_BODY["data"] = product
+    return RESPONSE_BODY, 200
+
+
+@products.route('/product-stock/<int:id>')
+def get_product_stock(product_id):
+    pass
+
+
+@products.route('/need-restock')
+def get_products_that_need_restock():
+    pass
+
+
+@products.route('/register-product-stock/<int:id>', methods=['PUT', 'POST'])
+def register_product_refund_in_stock():
+    pass
 
 """Se utiliza para renderizar plantillas, 
     es decir despues de una peticion se puede lanzar una pagina o alguna plantilla rapidamente,
